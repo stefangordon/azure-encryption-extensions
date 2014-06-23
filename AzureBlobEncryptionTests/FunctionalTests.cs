6fb5d1c47f8fc0f3065939009775ef8f63a4ca5e
Stefan Gordon <stefan@keirgordon.com>
2014-06-23 16:37:21 -0700

    Initial commit.

---
+++ AzureBlobEncryptionTests/FunctionalTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureBlobEncryptionTests
+{
+    [TestClass]
+    public class FunctionalTests
+    {
+
+        [TestMethod]
+        public void UploadBlockBlob()
+        {
+
+            using (var file = new TemporaryFile(512))
+            {
+                CloudStorageAccount storageAccount = CloudStorageAccount.DevelopmentStorageAccount;
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+                CloudBlobContainer container = blobClient.GetContainerReference("testcontainer");
+
+                container.CreateIfNotExists();
+
+                CloudBlockBlob blob = container.GetBlockBlobReference(file.fileInfo.Name);
+
+                blob.UploadFromFile(file.fileInfo.FullName, FileMode.Open);
+
+            }
+
+        }
+
+    }
+}
---
+++ AzureBlobEncryptionTests/TemporaryFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBlobEncryptionTests
+{
+    class TemporaryFile : IDisposable
+    {
+        public FileInfo fileInfo { get; set; }
+
+        public TemporaryFile(int kilobytes)
+        {
+            if (kilobytes <= 0)
+                throw new ArgumentOutOfRangeException("Requested size must be greater than 0.");
+
+            string path = Path.GetTempFileName();
+
+            using (FileStream fs = File.OpenWrite(path))
+            {
+                Random random = new Random();
+                byte[] buffer = new byte[1024];
+
+                for (int i = 0; i < kilobytes; i++)
+                {
+                    random.NextBytes(buffer);
+                    fs.Write(buffer, 0, 1024);
+                }
+            }
+
+            fileInfo = new FileInfo(path);
+        }
+
+        public void Dispose()
+        {
+            fileInfo.Refresh();
+            if (fileInfo.Exists)
+            {
+                fileInfo.Delete();
+            }
+        }
+    }
+}
